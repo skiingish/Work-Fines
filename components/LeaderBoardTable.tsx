29c689f3ca1a474717efda61d3690760ae412b73
Sean <52271972+skiingish@users.noreply.github.com>
2024-03-06 09:57:25 -0700

    fixed the leader board

--- components/LeaderBoardTable.tsx
+++ components/LeaderBoardTable.tsx
@@ -1,82 +1,117 @@
-'use client'
+'use client';
 
-import { useState, useEffect } from "react"
+import { useState, useEffect } from 'react';
 import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-  } from "@/components/ui/table"
-  import { createClient } from "@/utils/supabase/client";
-
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { createClient } from '@/utils/supabase/client';
+import { Input } from './ui/input';
+import { set } from 'zod';
 
 export default function LeaderBoardTable() {
-    const supabase = createClient();
+  const supabase = createClient();
 
-    const [staffData, setStaffData] = useState<Staff[]>([])
+  const [staffData, setStaffData] = useState<Staff[]>([]);
+  const [filteredStaff, setFilteredStaff] = useState<Staff[]>(staffData);
 
-    useEffect(() => {
-        const fetchFines = async () => {
-            // fetch fines from supabase and join the matching staff members from the staff table
-            const {data: staff, error} = await supabase.from('staff').select(`
+  useEffect(() => {
+    const fetchFines = async () => {
+      // fetch fines from supabase and join the matching staff members from the staff table
+      const { data: staff, error } = await supabase
+        .from('staff')
+        .select(
+          `
             *
-          `).order('created_at', {ascending: false});
-            console.log(staff);
-            if (error) {
-                console.log('error', error)
-                return
-            }
+          `
+        )
+        .order('created_at', { ascending: false });
+      console.log(staff);
+      if (error) {
+        console.log('error', error);
+        return;
+      }
 
-            if (!staff) {
-                console.log('no fines')
-                return
-            }
+      if (!staff) {
+        console.log('no fines');
+        return;
+      }
 
-            // for each staff member, calculate the total fines outstanding
-            staff.forEach((staffMember: Staff) => {
-                staffMember.fines_outstanding = staffMember.fines_owed - staffMember.fines_paid
-            })
+      // for each staff member, calculate the total fines outstanding
+      staff.forEach((staffMember: Staff) => {
+        staffMember.fines_outstanding =
+          staffMember.fines_owed - staffMember.fines_paid;
+      });
 
-            // then order the staff members by the fines outstanding
-            staff.sort((a: Staff, b: Staff) => {
-                if (a.fines_outstanding && b.fines_outstanding) {
-                    return b.fines_outstanding - a.fines_outstanding;
-                }
-                return 0;
-            });
-
-            setStaffData(staff);
+      // then order the staff members by the fines outstanding in descending order
+      staff.sort((a: Staff, b: Staff) => {
+        if (
+          a.fines_outstanding !== undefined &&
+          b.fines_outstanding !== undefined
+        ) {
+          return b.fines_outstanding - a.fines_outstanding;
         }
-        fetchFines()
-    }, [])
-    
-    return (
-        <div>
-            <Table>
-  {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
-  <TableHeader>
-    <TableRow>
-      <TableHead className="w-[100px]">Who</TableHead>
-      <TableHead className="w-[100px]">Total Fines Owing</TableHead>
-      <TableHead>Total Fines - All Time</TableHead>
-    </TableRow>
-  </TableHeader>
+        return 0;
+      });
 
-<TableBody>
-    {staffData.map((staff: Staff) => {
-        return (
-            <TableRow key={staff.id}>
+      console.log(staff);
+
+      // staff.sort((a: Staff, b: Staff) => {
+      //     if (a.fines_outstanding && b.fines_outstanding) {
+      //         return b.fines_outstanding - a.fines_outstanding;
+      //     }
+      //     return 0;
+      // });
+
+      setStaffData(staff);
+      setFilteredStaff(staff);
+    };
+    fetchFines();
+  }, []);
+
+  return (
+    <div>
+      <h2 className='w-full mb-4'>Leaderboard</h2>
+      {/* Search box */}
+      <Input
+        type='search'
+        className='w-full mb-4'
+        placeholder='Search for a staff member'
+        onChange={(e) => {
+          const search = e.target.value;
+          const filteredStaffList = staffData.filter((staff: Staff) => {
+            return staff.name.toLowerCase().includes(search.toLowerCase());
+          });
+          setFilteredStaff(filteredStaffList);
+        }}
+      />
+      <Table>
+        {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
+        <TableHeader>
+          <TableRow>
+            <TableHead className='w-[100px]'>Who</TableHead>
+            <TableHead className='w-[100px]'>Total Fines Owing</TableHead>
+            <TableHead>Total Fines - All Time</TableHead>
+          </TableRow>
+        </TableHeader>
+
+        <TableBody>
+          {filteredStaff.map((staff: Staff) => {
+            return (
+              <TableRow key={staff.id}>
                 <TableCell>{staff.name}</TableCell>
                 <TableCell>{staff.fines_owed - staff.fines_paid}</TableCell>
                 <TableCell>{staff.fines_owed}</TableCell>
-            </TableRow>
-        );
-    })}
-</TableBody>
-</Table>
-        </div>
-    )
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
